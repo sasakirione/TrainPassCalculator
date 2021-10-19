6c230578f299d6ac88f93636b45f58524973396f
sasakirione <sasakirione@gmail.com>
2021-10-19 21:25:38 +0900

    re: 営業キロ数から定期運賃を求められるようにした

--- src/main/scala/Section.scala
+++ src/main/scala/Section.scala
@@ -1,18 +1,21 @@
+import scala.math.abs
+
 case class Section(from: String, to: String) extends SectionInterface {
-  private var sectionPrice = ((8900, 3200) , (5080, 2040))
+  private var sectionPrice = List((4, 4700, 1870), (6, 5080, 2040), (9, 5890, 2270), (12, 6680, 2620), (15, 7430, 2860), (19, 8990, 3200))
+  private var salesDistance = List(("新宿", 0.0), ("明大前",5.2 ), ("武蔵野台",18.8))
+  private var fromDouble:Double = salesDistance.filter(s => s._1.equals(from)).last._2
+  private var toDouble:Double = salesDistance.filter(s => s._1.equals(to)).last._2
+  private var distance = abs(toDouble - fromDouble)
 
   def normalCommutePrice(i: Int): Int = calculatePrice(i, NormalTrainPass)
 
   def schoolCommutePrice(i: Int): Int = calculatePrice(i, SchoolTrainPass)
 
   def calculatePrice(i: Int, passType: TrainPassType): Int = {
-    var price = to match {
-      case "武蔵野台" => sectionPrice._1
-      case "明大前" => sectionPrice._2
-    }
+    var price = sectionPrice.filter(p =>  distance <= p._1).head
     passType match {
-      case NormalTrainPass => price._1
-      case SchoolTrainPass => price._2
+      case NormalTrainPass => price._2
+      case SchoolTrainPass => price._3
     }
   }
 
--- src/test/scala/TicketTest.scala
+++ src/test/scala/TicketTest.scala
@@ -5,17 +5,17 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class TicketTest extends AnyFunSuite {
   test("新宿武蔵野台の通勤1ヶ月定期代を表示する") {
-    var ticket = new Section("新宿", "武蔵野台")
-    ticket.normalCommutePrice(1) should be (8900)
+    var ticket = Section("新宿", "武蔵野台")
+    ticket.normalCommutePrice(1) should be (8990)
   }
 
   test("新宿明大前の通勤3ヶ月定期代を表示する") {
-    var ticket = new Section("新宿", "明大前")
+    var ticket = Section("新宿", "明大前")
     ticket.normalCommutePrice(1) should be (5080)
   }
 
   test("新宿武蔵野台の通学1ヶ月定期代を表示する") {
-    var ticket = new Section("新宿", "武蔵野台")
+    var ticket = Section("新宿", "武蔵野台")
     ticket.schoolCommutePrice(1) should be (3200)
   }
 }
